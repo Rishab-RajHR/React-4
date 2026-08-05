b13b0a7fb1c7aeff2ee173b28a0a2dbf73fcce6a
Rishab Raj <rr0948555@gmail.com>
2026-08-05 11:27:49 +0530

    ClassComponent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FuncComponent from './FuncComponent'
+import ClassComponent from './ClassComponent'
 
 const App = () => {
   return (
     <div>
         <h1>App</h1>
         <FuncComponent/>
+        <ClassComponent />
     </div>
   )
 }
