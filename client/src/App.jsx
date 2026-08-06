246cd0a4075f0e3705aed8b04cbe986bd394efc3
Rishab Raj <rr0948555@gmail.com>
2026-08-06 13:36:48 +0530

    Use Effect Hook with condition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-import Hooks from './Hooks'
-import UseEffect from './UseEffect'
-import UseEffectProps from './UseEffectProps'
+import UseEffectConditions from './UseEffectConditions'
 
 const App = () => {
+  const [age, setAge] = useState(22);
   const [count, setCount] = useState(0);
   return (
     <div>
         <h1>App</h1>
-        <UseEffectProps data={count} />
-        <button onClick={() => setCount(count + 1)}>Counter</button>
+        <UseEffectConditions age={age} count={count} />
+        <button onClick={() => setAge(age + 1)}>Update Age</button>
+        <button onClick={() => setCount(count + 1)}>Update Count</button>
     </div>
   )
 }
---
+++ client/src/UseEffectConditions.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+
+const UseEffectConditions = (props) => {
+  // const [age, setAge] = useState(22);
+  // const [count, setCount] = useState(0);
+
+  // useEffect With State
+  // useEffect(() => {
+  //     console.log("UseEffect Hook With Count " ,count)
+  // }, [count])
+  // useEffect(() => {
+  //     console.log("UseEffect Hook With Age State ", age)
+  // }, [age])
+
+
+  // useEffect With Props
+  useEffect(() => {
+      console.log("UseEffect Hook With Count ", props.count)
+  }, [props.count])
+
+  useEffect(() => {
+      console.log("UseEffect hook With Age ", props.age)
+  }, [props.age])
+
+
+  return (
+    <>
+       <h2> UseEffect Hook Specific States & Props </h2>
+       {/* <h3>Age: {age} </h3>
+       <h3>Count: {count} </h3>
+       <button onClick={() => setAge(age + 1)}>Update Age</button>
+       <button onClick={() => setCount(count + 1)}>Update Count</button> */}
+
+       <h3>Age: {props.age} </h3>
+       <h3>Count: {props.count} </h3>
+    </>
+  )
+}
+
+export default UseEffectConditions
