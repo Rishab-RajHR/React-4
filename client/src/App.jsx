ab391615b58249675563a987e007393f7e6e518d
Rishab Raj <rr0948555@gmail.com>
2026-08-07 18:29:14 +0530

    Axios Delete Method

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import AxiosPutRequest from './Axios/AxiosPutRequest'
+import AxiosDeleteRequest from './Axios/AxiosDeleteRequest'
 
 function App() {
 
   return (
     <div>
        <h2>App</h2>
-       <AxiosPutRequest />
+       <AxiosDeleteRequest />
     </div>
   )
 }
