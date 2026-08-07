ba8091fb87f1382ed535979abf90bc228d3d1e8a
Rishab Raj <rr0948555@gmail.com>
2026-08-07 12:02:55 +0530

    previous props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import PreviousState from './PreviousState'
+import React, { useState } from 'react'
+import PreviousProps from './PreviousProps'
 
 
 function App() {
+  const [data, setData] = useState(0);
+  function updateData () {
+      // setData(data + 1)
+      let randomValue = Math.floor(Math.random()*10)
+      setData(randomValue)
+  }
   return (
     <div>
        <h2>App</h2>
-       <PreviousState />
+       <PreviousProps item={data} />
+       <button onClick={updateData}>Update</button>
     </div>
   )
 }
