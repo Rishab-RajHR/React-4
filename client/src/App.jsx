b7bfba71d2f5cedb53eb2920dff8e35ee2756fdf
Rishab Raj <rr0948555@gmail.com>
2026-08-07 17:57:02 +0530

    Axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import AxiosGetRequest from './Axios/AxiosGetRequest'
+import AxiosPostRequest from './Axios/AxiosPostRequest'
 
 function App() {
 
   return (
     <div>
        <h2>App</h2>
-       <AxiosGetRequest />
+       <AxiosPostRequest />
     </div>
   )
 }
