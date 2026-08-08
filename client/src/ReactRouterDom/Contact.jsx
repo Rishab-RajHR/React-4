636d512c475753cc602ee8c70f20b154899c6e5f
Rishab Raj <rr0948555@gmail.com>
2026-08-08 14:15:24 +0530

    Usenavigate

--- client/src/ReactRouterDom/Contact.jsx
+++ client/src/ReactRouterDom/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+      let bool = true;
+      if(bool) {
+          navigate('/')
+      } else {
+         navigate('/about')
+      }
+  }
   return (
     <>
         <h2>Contact</h2>
+        {/* <button onClick={() => navigate('/')}>Go To Home Page</button> */}
+         <button onClick={() => handleClick()}>Go To Home Page</button>
+        <br/>
+        <br/>
+        {/* <button onClick={() => navigate('/about')}>Go To About Page</button> */}
+         <button onClick={() => handleClick()}>Go To About Page</button>
     </>
   )
 }
