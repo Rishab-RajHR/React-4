7659c96992520e62613f4fbc89ace36dff82f0b4
Rishab Raj <rr0948555@gmail.com>
2026-08-08 10:26:56 +0530

    Navbar and Link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './ReactRouterDom/Home';
 import About from './ReactRouterDom/About';
+import Navbar from './ReactRouterDom/Navbar';
 function App() {
 
   return (
     <div>
        <h2>App</h2>
        <BrowserRouter>
+         <Navbar />
           <Routes>
                <Route path='/home' element={<Home />} />
                <Route path='/about' element={<About />} />
