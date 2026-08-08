f9c0d09088f52070c85e9c409b640d4e83528d83
Rishab Raj <rr0948555@gmail.com>
2026-08-08 14:40:57 +0530

    Nested Routing and Outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,10 @@ import Contact from './ReactRouterDom/Contact';
 
 import './App.css';
 import Search from './ReactRouterDom/Search';
+import Company from './ReactRouterDom/Company';
+import Admin from './ReactRouterDom/Admin';
+import Faculty from './ReactRouterDom/Faculty';
+import Student from './ReactRouterDom/Student';
 
 function App() {
 
@@ -21,6 +25,11 @@ function App() {
                <Route path='/about' element={<About />} />
                <Route path='/contact' element={<Contact />}/>
                <Route path='/search' element={<Search />}/>
+              <Route path='/company' element={<Company />}> 
+                   <Route path='admin' element={<Admin />}/>
+                   <Route path='faculty' element={<Faculty />}/>
+                   <Route path='student' element={<Student />}/>
+              </Route>
                <Route path='/employee/:name/:age' element={<Employee />}/>
                {/* <Route path='/*' element={<PageNotFound />} /> */}
                <Route path='/*' element={<Navigate to='/' />} /> 
--- client/src/ReactRouterDom/Navbar.jsx
+++ client/src/ReactRouterDom/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
         <li><NavLink className='nav-link' to='/home'>Home</NavLink></li>
         <li><NavLink className='nav-link' to='/about'>About</NavLink></li>
         <li><NavLink className='nav-link' to='/contact'>Contact</NavLink></li>
+          <li><NavLink className='nav-link' to='/company'>Company</NavLink></li>
         <li><NavLink className='nav-link' to='/search'>Search</NavLink></li> 
      </ul>
     </>
